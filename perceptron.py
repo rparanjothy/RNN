cd1cc574c839669771e1d7f4a5e7c4570555ad4b
rkumarp <issac7@gmail.com>
2019-11-28 23:24:52 -0600

    updated weight tuning

--- perceptron.py
+++ perceptron.py
@@ -10,7 +10,7 @@ class Perceptron(object):
     o=[]
     weight=[]
     bias=0
-    lrate=0.1
+    lrate=0.01
 
     def __init__(self,i):
         # initialize weights when a perceptron is created.
@@ -25,7 +25,7 @@ class Perceptron(object):
   
     def activate(self):
         # randomly initialize weigths between 1 and -1
-        self.weight=[format(random.uniform(-1,1),"1.4f") for _ in self.i]
+        self.weight=[format(random.uniform(-1,1),"1.2f") for _ in self.i]
 
     def guess(self):
         # compute the weighted sum for input and map to 1 and -1
@@ -35,7 +35,7 @@ class Perceptron(object):
     def normalize(self):
         maxInput=max([abs(_) for _ in self.i])
         if maxInput>1 or abs(min(self.i)) > 1:
-            self.i=[format(float(_)/float(maxInput),'1.5f') for _ in self.i]
+            self.i=[format(float(_)/float(maxInput),'1.2f') for _ in self.i]
             print("*"*5)
             # print('Normalized inputs : {0}'.format(self.i))
 
@@ -43,20 +43,22 @@ class Perceptron(object):
     def train(self,target):
         # This method mutates the weights if guess and target doesnt match
         print("\tCurrent Input - {0}".format(self.i))
-        # print("\tCurrent weights - {0}".format(self.weight))
+        print("\tCurrent weights - {0}".format(self.weight))
         g=self.guess()
         error=float(g)-float(target)
         print ("\tError - {0} : Guess: {1} Target: {2}".format(error,g,target))
         # now tune weights per error -> weight = weight + error*each weight
-        if float(error) == 0.0:
+        if float(error) != 0.0:  
+            # increment if error is + and decrement if error is -ve
+            self.weight=map(lambda x:(float(x)*float(error)*float(self.lrate))+float(x),self.i)  if error < 0 else map(lambda x:(float(x)*float(error)*float(self.lrate))-float(x),self.i)
+            print("***** Tuning weights - {0}".format(self.weight))
+            self.train(target)
+        else:
             print("\tFinal weights : {0}".format(self.weight))
             return error
-        else:
-            self.weight=map(lambda x:(float(x)*float(error)*float(self.lrate))+float(x),self.i)
-            print("***** Tuning weights - {0}".format(self.weight))
-            return self.train(target)
+       
 
 
 if __name__ == "__main__":
-    p=Perceptron([.5,4.10])    
+    p=Perceptron([2,4,5])    
     p.train(1)
